1f7bb27560606fa79c4b796c312f07a9ca74cfe7
Jayson Wu <wulingjie991@outlook.com>
2021-02-07 23:30:16 +0800

    update packages and babel code

--- webpack.config.js
+++ webpack.config.js
@@ -3,35 +3,21 @@ const path = require('path')
 module.exports = {
   mode: 'production',
   entry: './src/index.js',
-  target: [ 'web', 'es5' ],
+  target: ['web', 'es5'],
   output: {
     filename: 'index.js',
     libraryTarget: 'commonjs2',
     path: path.resolve(__dirname, './lib')
   },
   resolve: {
-    extensions: [ '.js' ]
+    extensions: ['.js']
   },
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              [
-                '@babel/preset-env',
-                {
-                  targets: {
-                    node: 'current'
-                  }
-                }
-              ]
-            ]
-          }
-        }
+        use: ['babel-loader']
       }
     ]
   }
